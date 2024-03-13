a1e7ed2c8dc73478913945b12ea3d536d71f1b30
Adepegba David Ayobami <110990491+adepegba1@users.noreply.github.com>
2024-03-13 11:42:56 +0100

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,33 @@
 #!/usr/bin/python3
-"""Starts a Flask web application"""
-
+"""
+Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+/states_list: HTML page with a list of all State objects in DBStorage.
+"""
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-from flask import Flask
-from flask import render_template
 app = Flask(__name__)
-
-
-@app.route('/states_list', strict_slashes=False)
-def states():
-    """Returns a rendered html template
-    at the /states_list route,
-    listing all states"""
-    return render_template('7-states_list.html',
-                           states=storage.all('State').values())
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown(self):
-    """Removes the current SQLAlchemy Session"""
+def close_db(exc):
+    """close the current session of sqlalchemist"""
     storage.close()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+
+@app.route('/states_list')
+def states_list():
+    """
+    Displays an HTML page with a list of all State objects in DBStorage.
+    States are sorted by name.
+    """
+    states = storage.all(State).values()
+    return render_template("7-states_list.html", states=states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
